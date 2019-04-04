f4de81b36f6995a03e71178e11561d88b5296da2
RexSocrates <socrateshung053@gmail.com>
2019-04-04 14:22:06 +0800

    add a result class to record the experiment result

---
+++ ExpResult.java
@@ -0,0 +1,12 @@
+
+public class ExpResult {
+	double interaction = 0;
+	double signals = 0;
+	double successRate = 0;
+	
+	public ExpResult(double interaction, double signals, double successRate) {
+		this.interaction = interaction;
+		this.signals = signals;
+		this.successRate = successRate;
+	}
+}
