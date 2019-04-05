d7bf78cabc33c5227627d73262c3aeb12ee45dde
RexSocrates <socrateshung053@gmail.com>
2019-04-05 23:24:09 +0800

    set cycle time to 10 hr

--- GuReservationSimulator.java
+++ GuReservationSimulator.java
@@ -212,7 +212,7 @@ public class GuReservationSimulator {
     		// enter some variable that IRS needs
 //    		System.out.print("Enter data collection periods(hour 1 ~ 168) : ");
 //        	dataCollectionPeriods = input.nextDouble();
-    		dataCollectionPeriods = 5;
+    		dataCollectionPeriods = 10;
         	
         	// read period length file
     		/*
